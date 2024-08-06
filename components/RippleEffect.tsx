30735b397cc6b1b41539f7f2439a3a0b8384bbd5
Ridwan <ridwanyinusa15@gmail.com>
2024-08-06 12:49:16 +0100

    initial commit

---
+++ components/RippleEffect.tsx
@@ -0,0 +1,24 @@
+"use client";
+import React, { useEffect } from "react";
+import $ from "jquery";
+import "jquery.ripples";
+
+interface RippleEffectProps {
+  className?: string;
+  children: React.ReactNode;
+}
+
+const RippleEffect: React.FC<RippleEffectProps> = ({ className, children }) => {
+  useEffect(() => {
+    // Initialize jQuery Ripples effect
+    $(".ripple-container").ripples({
+      resolution: 512,
+      dropRadius: 20,
+      perturbance: 0.04,
+    });
+  }, []);
+
+  return <div className={`ripple-container ${className}`}>{children}</div>;
+};
+
+export default RippleEffect;
